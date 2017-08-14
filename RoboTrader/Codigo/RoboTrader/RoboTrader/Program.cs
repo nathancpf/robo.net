1bb033fdae45070dc0ada82d02d2f6c8e08f2975
andrehortab2u <30999942+andrehortab2u@users.noreply.github.com>
2017-08-14 07:20:32 -0300

    Permitindo escolher robo

--- RoboTrader/Codigo/RoboTrader/RoboTrader/Program.cs
+++ RoboTrader/Codigo/RoboTrader/RoboTrader/Program.cs
@@ -10,7 +10,27 @@ namespace RoboTrader
     {
         static void Main(string[] args)
         {
-            RoboTrader2 robo = new RoboTrader2();
+            Console.WriteLine("Bem vindo aos robo.net da Bitcointoyou!");
+            Console.WriteLine("Qual robô você deseja executar?");
+            Console.WriteLine("1 - Robo war");
+            Console.WriteLine("2 - Robo trader v2");
+            Console.WriteLine("3 - Robo trader v3");
+            Console.WriteLine("Digite um número de 1 a 3 e pressione enter.");
+
+            string opcao = Console.ReadLine();
+
+            if (opcao == "1")
+            {
+                RoboWar robo = new RoboWar();
+            }
+            else if (opcao == "2")
+            {
+                RoboTrader2 robo = new RoboTrader2();
+            }
+            else if (opcao == "3")
+            {
+                Robo3 robo = new Robo3();
+            }
         }
     }
 }
